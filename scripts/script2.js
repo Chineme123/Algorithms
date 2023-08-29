362e449df0c41ad75a613ee75babdd029a7d9996
chineme123 <chineme2101@gmail.com>
2023-08-29 06:39:50 +0100

    Bubble Sort Update

--- scripts/script2.js
+++ scripts/script2.js
@@ -6,7 +6,7 @@ function generateRandomArray(){
     }
 }
 
-async function bubbleSort(delay = 1000){
+async function bubbleSort(delay = 2000){
     for(let i = 0; i < arr.length; i++){
 
         for(let j = 0; j < arr.length - i - 1; j++){
@@ -14,30 +14,28 @@ async function bubbleSort(delay = 1000){
             arr[j].style.backgroundColor = "grey"
             arr[j + 1].style.backgroundColor = "grey"
 
-            await new Promise((resolve) =>
-                setTimeout(() => {
-                    resolve();
-                }, delay)
-            );
+            await new Promise((resolve) => setTimeout(() => { resolve(); }, delay));
 
-            if (Number(arr[j].textContent) > Number(arr[j + 1].textContent)) {
+            if (parseInt(arr[j].textContent) > parseInt(arr[j + 1].textContent)) {
+
                 const temp = arr[j].textContent;
                 arr[j].textContent = arr[j + 1].textContent;
                 arr[j + 1].textContent = temp;
                 
-                // arr[j].style.transition = "transform 1s ease-in-out";
-                // arr[j + 1].style.transition = "transform 1s ease-in-out";
-                // arr[j].style.transform = "translate(calc((100vw - 500px)/7))";
-                // arr[j + 1].style.transform = "translate(calc((100vw - 500px)/-7))"; 
-                // await new Promise((resolve) =>
-                //     arr[j].addEventListener("transitionend", resolve, { once: true })
-                // );
-                // arr[j].style.transform = "none";
-                // arr[j + 1].style.transform = "none";
+                arr[j].style.transform = "translate(calc((100vw - 500px)/7))";
+                arr[j + 1].style.transform = "translate(calc((100vw - 500px)/-7))"; 
+
+                await new Promise((resolve) =>
+                    arr[j].addEventListener("transitionend", resolve, { once: true })
+                );
+
+                arr[j].style.transform = 'none';
+                arr[j + 1].style.transform = 'none';
+                
             }
 
-            for(let i = 0; i < arr.length; i++){
-                arr[i].style.backgroundColor = "white"
+            for(let k = 0; k < arr.length; k++){
+                arr[k].style.backgroundColor = "white"
             }
             
         }
